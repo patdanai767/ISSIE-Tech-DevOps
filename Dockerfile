3b3d93a1d51e0decd1c07e2c6f219feb4e5fa154
Patrdanai Chumrus <pattproc@gmail.com>
2025-03-13 18:45:07 +0700

    feat: dockerfile and compose up and fix hardcode

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:20-alpine AS base
+
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm install
+
+FROM node:20-alpine AS builder
+
+WORKDIR /app
+COPY . .
+COPY --from=base /app/node_modules ./node_modules
+RUN npm run build
+
+FROM node:20-alpine AS runner
+
+WORKDIR /app
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/package.json ./package.json
+COPY --from=builder /app/dist ./dist
+COPY .env .env
+
+EXPOSE 8080
+
+CMD [ "node", "dist/main.js"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:example@mongo:27017')],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
